f63d4c3061297421cb83bf0486fb8f1ac432e833
andriideikalo <105654347+andriideikalo@users.noreply.github.com>
2022-09-24 23:19:03 +0300

    x

--- js/exemple.js
+++ js/exemple.js
@@ -1,30 +1,42 @@
 
-function checkPassword(password) {
-    password = 2;
-    const ADMIN_PASSWORD = "jqueryismyjam";
-    let message;
-       // Change code below this line
-  
-    // if (password === null) {
-    //   message = "Canceled by user!";
-    // } else if (password === ADMIN_PASSWORD) {
-    //   message = "Welcome!";
-    // } else {
-    //   message = "Access denied, wrong password!";
-    // }
-  
-    switch (password) {
-        case null:
-            message = `Canceled by user!`;
-            break;
-        case ADMIN_PASSWORD:
-            message = `Welcome!`;
-            break;
-        default:
-            message = `Access denied, wrong password!`;
-      
-    }
-  
+// function checkPassword(password) {
+//     password = 2;
+//     const ADMIN_PASSWORD = "jqueryismyjam";
+//     let message;
+//        // Change code below this line
+
+//     // if (password === null) {
+//     //   message = "Canceled by user!";
+//     // } else if (password === ADMIN_PASSWORD) {
+//     //   message = "Welcome!";
+//     // } else {
+//     //   message = "Access denied, wrong password!";
+//     // }
+
+//     switch (password) {
+//         case null:
+//             message = `Canceled by user!`;
+//             break;
+//         case ADMIN_PASSWORD:
+//             message = `Welcome!`;
+//             break;
+//         default:
+//             message = `Access denied, wrong password!`;
+
+//     }
+
+//     // Change code above this line
+//     return message;
+// }
+
+
+let string = "The quick brown fox jumped over the lazy dog";
+
+function findLongestWord(string) {
+    // Change code below this line
+    let allwolrds = string.split(" ");
+
+    return allwolrds;
+
     // Change code above this line
-    return message;
 }
