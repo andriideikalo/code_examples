bd1166c5971ce0a24e8a83ac99c3390775c890be
andriideikalo <105654347+andriideikalo@users.noreply.github.com>
2022-09-25 19:29:37 +0300

    x

--- js/exemple.js
+++ js/exemple.js
@@ -28,15 +28,117 @@
 //     // Change code above this line
 //     return message;
 // }
+// const user = {
+//     name: "Jacques Gluke",
+//     tag: "jgluke",
+//     location: {
+//       country: "Jamaica",
+//       city: "Ocho Rios",
+//     },
+//     hobbies: ["swiming", "music", "sci-fi"],
+//   };
+  
+//   let loc = user.location.city;
+//   console.log(loc); // Объект location
+  
+//   const country = user.location.country;
+//   console.log(country); // 'Jamaica'
+
+//   const hobbies = user.hobbies;
+// console.log(hobbies); // ['swiming', 'music', 'sci-fi']
+
+// const firstHobby = user.hobbies[0];
+// console.log(firstHobby); // 'swiming'
+
+// const numberOfHobbies = user.hobbies.length;
+// console.log(numberOfHobbies); // 3
+
+// const book = {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["historical prose", "adventure"],
+//     isPublic: true,
+//     rating: 8.38,
+//   };
+  
+//   const bookTitle = book["title"];
+//   console.log(bookTitle); // 'The Last Kingdom'
+  
+//   const bookGenres = book["genres"];
+//   console.log(bookGenres); // ['historical prose', 'adventurs']
+  
+// book.author = "andrii";
+
+//   const propKey = book["author"];
+// //   const bookAuthor = book[propKey];
+// console.log(propKey); // 'Bernard Cornwell'
+// console.log(book);
+
+// book.name = "free";
+// console.log(book);
+
+// // console.log(book.rating);
+// const book = {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["historical prose", "adventure"],
+//     rating: 8.38,
+//   };
+  
+//   for (const key in book) {
+//     // Ключ
+//     console.table(key);
+//     // Значение свойства с таким ключом
+//     console.table(book);
+// }
+// const book = {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["historical prose", "adventure"],
+//     rating: 8.38,
+// };
+// const dog = Object.create(book);
+// dog.name = "kola";
+  
+//   for (const key in book) {
+//     // Если это собственное свойство - выполняем тело if
+//     if (1 > 0) {
+//       console.log(key);
+        // console.log(dog.title);
+        // console.log(dog.name);
+    // }
+  
+    // Если это не собственное свойство - ничего не делаем
+//   }
 
 
-let string = "The quick brown fox jumped over the lazy dog";
+const books = [
+    {
+      title: "The Last Kingdom",
+      author: "Bernard Cornwell",
+      rating: 8.38,
+    },
+    {
+      title: "На берегу спокойных вод",
+      author: "Роберт Шекли",
+      rating: 8.51,
+    },
+    {
+      title: "Сон смешного человека",
+      author: "Федор Достоевский",
+      rating: 7.75,
+    },
+];
 
-function findLongestWord(string) {
-    // Change code below this line
-    let allwolrds = string.split(" ");
 
-    return allwolrds;
 
-    // Change code above this line
+
+let bookNames = 0;
+
+for (const book of books) {
+    bookNames += book.rating;
+    
 }
+let total = (bookNames / books.length).toFixed(3); 
+
+console.log(total);
